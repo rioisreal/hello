562f2fc8df7a721e6819409cb5dde7104ad913fb
rio <rio132@163.com>
2021-08-14 16:33:32 +0800

    version 2

--- src/com/home/java/gittest.java
+++ src/com/home/java/gittest.java
@@ -3,6 +3,7 @@ package com.home.java;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 
 }
